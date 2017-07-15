998c50a0b1c1d7c296e30791ca545764fc562c05
usuario <usuario@user>
2017-07-14 23:43:04 -0500

    junit establecimiento

---
+++ src/test/java/com/reservappfinal/entrega/modelo/control/EstablecimientoLogicTest.java
@@ -0,0 +1,36 @@
+package com.reservappfinal.entrega.modelo.control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.reservappfinal.entrega.modelo.Establecimiento;
+
+public class EstablecimientoLogicTest {
+
+	@Test
+	public void testSaveEstablecimiento() {
+		//fail("Not yet implemented");
+		Establecimiento estable = new Establecimiento();
+		estable.setCodigoEstablecimiento(12);
+		estable.setCorreo("super@coco.es");
+		estable.setDireccion("barrio cristal");
+		estable.setLatitud("23");
+		estable.setLongitud("12");
+		estable.setMesas(11);
+		estable.setNit("11");
+		estable.setNombre("nombre al");
+		estable.setTelefono("318095866");
+		
+		EstablecimientoLogic esta=new EstablecimientoLogic();
+		try {
+			esta.saveEstablecimiento(estable);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
